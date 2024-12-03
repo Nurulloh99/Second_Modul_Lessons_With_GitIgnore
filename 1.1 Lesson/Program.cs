a15d069e151b87c147da60f8f2ac2d0ef37d4606
Nurulloh99 <nooroonus@gmail.com>
2024-12-03 09:11:52 +0500

    Second modul lessons have been started successfully

---
+++ 1.1 Lesson/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1._1_Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
