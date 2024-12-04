36ea183056df40d92b6deeb357a0bec28c8c84f6
Nurulloh99 <nooroonus@gmail.com>
2024-12-04 20:29:02 +0500

    String Builder has been formulated

---
+++ 1.1 Lesson/My StringBuilder/ClassStringBuilder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1_Lesson.My_StringBuilder;
+
+public class ClassStringBuilder
+{
+    private char[] strBuilder;
+
+    private int defCapacity = 16;
+    private int index = 0;
+
+    public ClassStringBuilder()
+    {
+        strBuilder = new char[defCapacity];
+    }
+
+    public ClassStringBuilder(int capasity)
+    {
+        strBuilder = new char[capasity];
+    }
+
+    public void AppendValue(string strAppend)
+    {
+        if(index + strAppend.Length <= strBuilder.Count())
+        {
+            foreach (var strChar in strAppend)
+            {
+                strBuilder[index] = strChar;
+                index++;
+            }
+        }
+        else
+        {
+            var newChar = new char[strBuilder.Length * 2];
+
+            index = 0;
+
+            foreach(var strChar in strBuilder)
+            {
+                newChar[index] = strChar;
+                index++;
+            }
+
+            foreach (var strChar in strAppend)
+            {
+                newChar[index] = strChar;
+                index++;
+            }
+
+            strBuilder = newChar;
+        }
+    }
+
+     
+    public string GetValueBackStringFromArray()
+    {
+        return string.Join("", strBuilder);
+    }
+
+
+    public void UpdateValue(int index, char character)
+    {
+        strBuilder[index] = character;
+    }
+}
